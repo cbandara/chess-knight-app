25f047455222d81a9ea77f94328411ae690f78bc
devin23 <devin@3droomspace.com>
2020-06-09 14:13:46 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Chessboard API Project`,
+    description: `An experiment that displays the possible moves of a Knight using chessboard.jsx`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Chessboard from "chessboardjsx";
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Chessboard position="start" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
